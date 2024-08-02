b3e4c61404759edb32cf680a869027c6fddb2c00
Arikan.Li <panlina68@hotmail.com>
2024-08-02 12:43:59 +0800

    fix:    android ndk tools only named x86_64 arch dir for os

--- apex-toolchain/toolchain-android.cmake
+++ apex-toolchain/toolchain-android.cmake
@@ -5,13 +5,7 @@ set(CMAKE_SYSTEM_NAME Android)
 
 # Refer to CMAKE_SYSTEM_PROCESSOR to dynamically set CMAKE_SYSTEM_PROCESSOR & CMAKE_ANDROID_ARCH_ABI
 if(CMAKE_SYSTEM_PROCESSOR STREQUAL "arm64-v8a")
-    if(CMAKE_HOST_SYSTEM_NAME STREQUAL "Linux")
-        set(CMAKE_SYSTEM_PROCESSOR arm64-v8a)
-    elseif(CMAKE_HOST_SYSTEM_NAME STREQUAL "Windows")
-        set(CMAKE_SYSTEM_PROCESSOR armv8-a)
-    elseif(CMAKE_HOST_SYSTEM_NAME STREQUAL "Darwin")
-        set(CMAKE_SYSTEM_PROCESSOR aarch64)
-    endif()
+    set(CMAKE_SYSTEM_PROCESSOR aarch64)
     set(CMAKE_ANDROID_ARCH_ABI arm64-v8a)
     set(TARGET_TRIPLE "aarch64-none-linux-android")
 elseif(CMAKE_SYSTEM_PROCESSOR STREQUAL "armeabi-v7a")
@@ -32,18 +26,20 @@ endif()
 
 # Set the correct compiler paths based on the host operating system and processor
 if(CMAKE_HOST_SYSTEM_NAME STREQUAL "Linux")
-    set(ANDROID_TOOLCHAIN_PATH ${ANDROID_NDK}/toolchains/llvm/prebuilt/linux-${CMAKE_HOST_SYSTEM_PROCESSOR})
+    set(ANDROID_TOOLCHAIN_PATH ${ANDROID_NDK}/toolchains/llvm/prebuilt/linux-x86_64)
 elseif(CMAKE_HOST_SYSTEM_NAME STREQUAL "Windows")
-    set(ANDROID_TOOLCHAIN_PATH ${ANDROID_NDK}/toolchains/llvm/prebuilt/windows-${CMAKE_HOST_SYSTEM_PROCESSOR})
+    set(ANDROID_TOOLCHAIN_PATH ${ANDROID_NDK}/toolchains/llvm/prebuilt/windows-x86_64)
 elseif(CMAKE_HOST_SYSTEM_NAME STREQUAL "Darwin")
-    set(ANDROID_TOOLCHAIN_PATH ${ANDROID_NDK}/toolchains/llvm/prebuilt/darwin-${CMAKE_HOST_SYSTEM_PROCESSOR})
+    set(ANDROID_TOOLCHAIN_PATH ${ANDROID_NDK}/toolchains/llvm/prebuilt/darwin-x86_64)
 else()
     message(FATAL_ERROR "Unsupported CMAKE_HOST_SYSTEM_NAME: ${CMAKE_HOST_SYSTEM_NAME}")
 endif()
 
+message(STATUS "Found compiler: clang at ${ANDROID_TOOLCHAIN_PATH}")
+
 set(CMAKE_C_COMPILER ${ANDROID_TOOLCHAIN_PATH}/bin/clang)
 set(CMAKE_CXX_COMPILER ${ANDROID_TOOLCHAIN_PATH}/bin/clang++)
-
+find_library(log-lib log)
 # Set appropriate flags for the target architecture
 set(CMAKE_C_FLAGS "-target ${TARGET_TRIPLE}${CMAKE_SYSTEM_VERSION} --sysroot=${ANDROID_TOOLCHAIN_PATH}/sysroot" CACHE STRING "" FORCE)
 set(CMAKE_CXX_FLAGS "-target ${TARGET_TRIPLE}${CMAKE_SYSTEM_VERSION} --sysroot=${ANDROID_TOOLCHAIN_PATH}/sysroot" CACHE STRING "" FORCE)
